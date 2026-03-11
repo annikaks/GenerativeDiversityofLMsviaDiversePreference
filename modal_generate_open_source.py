61158d93fb440a4b22b1e5f42bda18cc0108d0b4
Shyamsaibethina <shyamsaibethina@gmail.com>
2026-03-10 18:24:13 -0700

    ignore

--- modal_generate_open_source.py
+++ modal_generate_open_source.py
@@ -28,7 +28,7 @@ image = (
     modal.Image.debian_slim(python_version="3.11")
     .apt_install("git")
     .pip_install_from_requirements("requirements.txt")
-    .add_local_dir(".", remote_path=str(PROJECT_ROOT))
+    .add_local_dir(".", remote_path=str(PROJECT_ROOT), ignore=[".git", "__pycache__", ".env"])
 )
 
 
